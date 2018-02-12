ac7e98e6c53cb79ed31c23afa36f4e497a6d80a0
splinterific <markgpreston@gmail.com>
2018-02-12 22:19:32 +0000

    updated with readme file, brief and first read in lines

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace HorseBetTracking
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please hold, working!");
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "commit_changes.txt");
+            var fileContents = ReadFile(fileName);
         }
     }
 }
